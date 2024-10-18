aa98a655c975e6b0972d0518d843f38b643887cd
felix Valerio <fvalerio@syneteksolutions.com>
2024-10-18 09:23:33 -0400

    Fix issue with class naming on command delete old logs

--- src/DataDog/AuditBundle/Command/DeleteOldLogsCommand.php
+++ src/DataDog/AuditBundle/Command/DeleteOldLogsCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class AuditLogDeleteOldLogsCommand extends Command
+class DeleteOldLogsCommand extends Command
 {
     public const DEFAULT_RETENTION_PERIOD = 'P6M';
 
